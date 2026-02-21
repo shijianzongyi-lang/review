e141dda8468b8a6749e79bb771620e2a246ef3a3
Junaco <shijianzongyi@gmail.com>
2026-02-21 16:42:36 +0900

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 'use strict';
 
-console.log('あはは');
+const client = supabase.createClient('https://ltwcysyrojosazncwkqf.supabase.co', 'sb_publishable_j5UkkULVm0FJQboPMoifaA_6Z9feuzz')
+
+//const SUPABASE_URL = "https://ltwcysyrojosazncwkqf.supabase.co";
+//const SUPABASE_ANON_KEY = "sb_publishable_j5UkkULVm0FJQboPMoifaA_6Z9feuzz";
+//const supabaseClient = supabase.createClient(
+//  SUPABASE_URL,
+//  SUPABASE_ANON_KEY
+//);
+
+async function loadReviews() {
+  const { data, error } = await client
+  .from('table_1')   // ← テーブル名
+  .select('*')
+
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+  const reviews = document.getElementById("review");
+  reviews.innerHTML = "";
+
+  data.forEach(review => {
+    const el = document.createElement("div");
+    el.innerHTML = `
+      <b>${review.user_name}</b><br>
+      ${review.stars}　${review.review}
+      <hr>
+    `;
+    reviews.appendChild(el);
+    console.log(review.stars);
+  })
+
+}
+
+loadReviews();
