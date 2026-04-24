b7873abd2a0658daba33f1422c7993a55c9d8a90
Junaco <shijianzongyi@gmail.com>
2026-04-24 10:47:52 +0900

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 const { createClient } = supabase
-const client = createClient('https://ltwcysyrojosazncwkqf.supabase.co', ${{ secrets.PUBLISH_KEY }})
+const client = createClient('https://ltwcysyrojosazncwkqf.supabase.co', 'sb_publishable_j5UkkULVm0FJQboPMoifaA_6Z9feuzz')
 const storage = localStorage;
 if (storage.getItem("good_num") == null) {
   //storage.good_num = JSON.stringify([]);//good_numの初期リスト設定
